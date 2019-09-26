254266b369cff68cbebe2073d6dd38347db757b2
raxits <savaliaraxit07@gmail.com>
2019-09-27 00:07:42 +0530

    Revert "Index Array"
    This reverts commit b04c8b1e833ff03bec99e7e5e1eefd323030179e.

--- 1 index array/ex2.php
+++
@@ -1,7 +0,0 @@
-<?php
-    $a = array("January","February","March","April","May","June","July","August","September","October","November","December");
-
-    for($i=0;$i<12;$i++){
-        echo $a[$i],"<br>";
-    }
-?>
