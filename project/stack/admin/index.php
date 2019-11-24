894c88c0f3c90d18fb136b2704ce832d11d08432
raxits <savaliaraxit07@gmail.com>
2019-11-24 22:29:44 +0530

    phpmyadmin

--- project/stack/admin/index.php
+++ project/stack/admin/index.php
@@ -5,7 +5,43 @@ if(!file_exists($dir) && !is_dir($dir)){
 }
 
 
+$sname = "localhost";
+$uname = "root";
+$pass = "";
+
+$con = new mysqli($sname,$uname,$pass);
+
+if($con->connect_error){
+    echo "Connection Fail<br>";
+}
+else{
+    $sql = "CREATE DATABASE if not exists stack";
+
+    if(!$con->query($sql) == TRUE){
+       echo "Database Created unSuccessfull<br>";
+    }
+    $con->close();
+
+    $db = "stack";
+    $con = new mysqli($sname, $uname, $pass, $db);
+
+    if($con->connect_error){
+        echo "Database Connection Fail<br>";
+    }
+    $s = "DESCRIBE admin";
+    if(!$con->query($s)){
+      $sql = "CREATE TABLE admin (id INT AUTO_INCREMENT, name VARCHAR(30), pwd VARCHAR(30), email VARCHAR(30), country VARCHAR(20), gender VARCHAR(10), knownlang TEXT, profileimg TEXT, PRIMARY KEY(id))";
+
+      if($con->query($sql) == True){
+          echo "Table Created Successfull<br>";
+      }
+      $con->close();  
+    }
+    
+}
 ?>
+
+
 <!--main-container-part-->
 <div id="content">
 <!--breadcrumbs-->
